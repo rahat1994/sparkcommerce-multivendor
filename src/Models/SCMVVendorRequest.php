281632aa2c410a4412d4af24c760513c994e7ad7
Rahat Baksh <rahat392@gmail.com>
2024-05-16 19:46:25 +0600

    chore: Add database migrations and models for Sparkcommerce Multivendor

---
+++ src/Models/SCMVVendorRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Rahat1994\SparkcommerceMultivendor\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SCMVVendor extends Model
+{
+    protected $fillable = ['name', 'email', 'meta', 'user_id'];
+
+    protected $casts = [
+        'name' => 'string',
+        'user_id' => 'integer',
+    ];
+
+    /**
+     * Get the table associated with the model.
+     *
+     * @return string
+     */
+    public function getTable()
+    {
+        return config('sparkcommerce-multivendor.table_prefix') . 'vendors';
+    }
+}
